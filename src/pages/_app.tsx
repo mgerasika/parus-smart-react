24d66c92d14aa085326959aa7de8e0da907eaf58
Mykhaylo Gerasika <mger@ciklum.com>
2022-01-01 08:30:16 +0200

    1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,8 @@ import { AppProps } from 'next/app';
 import Script from 'next/script';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { StickyContainer } from 'react-sticky';
 import '../scss/styles.scss';
 
 const queryClient = new QueryClient();
@@ -11,7 +12,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
             <Script id="scripts.js" src="scripts.js" />
-            <Component {...pageProps} />
+            <StickyContainer>
+                <Component {...pageProps} />
+            </StickyContainer>
             <Script id="bootstrap.js" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
             {/* The rest of your application */}
             <ReactQueryDevtools initialIsOpen={false} />
